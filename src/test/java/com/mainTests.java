c09a912a5b724d27ba1b47b455a0de2886fb9627
Pyr0Joke <37038004+Pyr0Joke@users.noreply.github.com>
2018-10-08 14:40:13 +0300

    test_4

--- src/test/java/com/mainTests.java
+++ src/test/java/com/mainTests.java
@@ -6,6 +6,6 @@ import org.testng.annotations.Test;
 public class mainTests {
     @Test
     public void first_test(){
-        Assert.assertEquals(5, 2+3);
+        Assert.assertEquals(5, 2+4);
     }
 }
